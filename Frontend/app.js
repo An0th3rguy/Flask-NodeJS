69be22ccb4e64b8713206e2cdfd3905ceefc4217
jakubferet <jakub.feret@seznam.cz>
2023-03-23 09:42:29 +0100

    fetch-update

--- Frontend/app.js
+++ Frontend/app.js
@@ -4,14 +4,14 @@ const hostname = '0.0.0.0';
 const port = 80;
 
 async function getData(url){
-  const response = await fetch("http://backend:5000");
+  const response = await fetch(url);
   return await response.json();
 }
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  getData("http://backend:5000").then(data =>{
+  getData("http://backend:5000").then(data => {
     console.log(data);
   });
 });
