a57b6a2e27bf470adc0eb7ee7e7b315a5bb4f827
jakubferet <jakub.feret@seznam.cz>
2023-03-29 09:09:05 +0000

    db update

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_json import FlaskJSON, json_response
 from flask_cors import CORS
 
@@ -7,7 +9,17 @@ app = Flask(__name__)
 FlaskJSON(app)
 CORS(app)
 
+connectionstring = 'postgresql+psycopg2://postgres:postgres@db:5432'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = connectionstring
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 @app.route('/')
 def hello_world():
     now = datetime.utcnow()
     return json_response(time=now)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
