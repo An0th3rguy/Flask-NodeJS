677b49540959119d0f070f79e92681543a716e54
An0th3rguy <anothermaily2k@gmail.com>
2023-05-17 12:03:20 +0000

    :)

--- Backend/flask_app/main/controller/registration_controller.py
+++ Backend/flask_app/main/controller/registration_controller.py
@@ -2,14 +2,14 @@ from flask import request
 from flask_restx import Resource
 
 from ..dto.user_dto import UserDto
-from ..service.user_service import save_new_user
+from ..service.user_service import save_new_user, login_post
 
 api = UserDto.apiRegistration
 # _user_get = UserDto.user_get
 _user_register = UserDto.user_register
 
 
-@api.route('/registration')
+@api.route('/')
 class Registration(Resource):
     @api.response(201, 'User successfully created.')
     @api.doc('create a new user')
@@ -19,3 +19,16 @@ class Registration(Resource):
         """Creates a new User """
         data = request.json
         return save_new_user(data=data)
+
+# api = UserDto.apiLogin
+_user_login = UserDto.user_login
+
+@api.route('/login')
+class Login(Resource):
+    @api.response(202, 'User login.')
+    @api.doc('login user')
+    @api.expect(_user_login, validate=True)
+
+    def post(self):
+        #user token
+        return login_post()  
--- Backend/flask_app/main/dto/user_dto.py
+++ Backend/flask_app/main/dto/user_dto.py
@@ -10,19 +10,23 @@ class UserDto:
                     cors.crossdomain(origin="http://localhost:3000")])
     apiDeletion = Namespace('deletion', description='deletion', decorators=[
                     cors.crossdomain(origin="http://localhost:3000")])
+    apiLogin = Namespace('registration', description='login', decorators=[
+                    cors.crossdomain(origin="http://localhost:3000")])
 
     user_register = apiRegistration.model('user_register',{
         'email': fields.String(required=True, description='user email address'),
         'password': fields.String(required=True, description='user password'),
         'name': fields.String(required=False, description='user first name'),
         'surname': fields.String(required=False, description='user last name')
-        # 'public_id': fields.Ïnteger(description='user Identifier')
+        # 'public_id': fields.Ïnteger(description='user Identifier'
+    }
+        )
 
-    user_login = apiLogin.model('user_login',{
+    user_login = apiRegistration.model('user_login',{
         'name': fields.String(required=True, description='user first name'),
         'password': fields.String(required=True, description='user password')
     })
-    })
+    
 
     user_delete = apiDeletion.model('user_delete',{
         'id' : fields.Integer(required=True, description='user id')
